22233b72223340727237c602c80713ac2a653f4b
Platinium <Platinium>
2024-05-12 18:43:10 +0200

    add validateTxProof

--- assembly/bitcoin.ts
+++ assembly/bitcoin.ts
@@ -1,6 +1,10 @@
 import { u128,u256 } from 'as-bignum/assembly'
-import { JSON } from 'assemblyscript-json/assembly'
 import * as Arrays from './common/arrays'
+import { BigInt } from "as-bigint/assembly";
+import { calcKey, extractPrevBlockLE, getHeaders, extractMerkleRootLE, hash256, getStringFromJSON } from './btc-relay-utils';
+import { JSON, JSONEncoder } from "assemblyscript-json/assembly";
+import { SystemAPI, db } from '.';
+import { Crypto } from './common/crypto';
 
 /**
  * @const {BigInt}
@@ -220,4 +224,203 @@ export function validateHeaderWork(digest: Uint8Array, target: Uint8Array): bool
       return false;
     }
     return Arrays.bytesToUint(Arrays.reverseEndianness(digest)) < Arrays.bytesToUint(target);
+}
+
+
+// pla: code below focuses on validating proofs
+
+function calcKey(height: i32): string {
+  const cs: i32 = 100;
+  // pla: is math.floor really necessary?
+  // const keyA: i32 = Mathf.floor(height / cs) * cs;
+  const keyA: i32 = (height / cs) * cs;
+
+  return keyA.toString() + "-" + (keyA + cs).toString();
+}
+
+function getHeaders(key: string): Map<i64, string> {
+  const pulledHeaders: Map<i64, string> = new Map<i64, string>();
+  const fetchedHeaderState = db.getObject(`headers/${key}`);
+  if (fetchedHeaderState !== "null") {
+      const parsed = <JSON.Obj>JSON.parse(fetchedHeaderState);
+      for (let i = 0; i < parsed.keys.length; ++i) {
+          let key = parsed.keys[i];
+          let blockRaw = getStringFromJSON(<JSON.Obj>parsed, key);
+          let height = parseInt(key) as i64;
+          pulledHeaders.set(height, blockRaw);
+      }
+  }
+
+  return pulledHeaders;
+}
+
+function getStringFromJSON(jsonObject: JSON.Obj, key: string): string {
+  let extractedValue: JSON.Str | null = jsonObject.getString(key);
+  if (extractedValue != null) {
+      return extractedValue.valueOf();
+  }
+
+  return "";
+}
+
+function extractPrevBlockLE(header: Uint8Array): Uint8Array {
+  return header.slice(4, 36);
+}
+
+function extractMerkleRootLE(header: Uint8Array): Uint8Array {
+  return header.slice(36, 68);
+}
+
+// Implements bitcoin's hash256 (double sha2)
+function hash256(preImage: Uint8Array): Uint8Array {
+  return Crypto.sha256(Crypto.sha256(preImage));
+}
+
+// pla: the below implementation was the only one working for me, trying with the existing one first
+// maybe the Crypto.sha256 function needs to be updated with the code below
+
+// function sha256(param: Uint8Array): Uint8Array {
+//   const arg0Value: string = Arrays.Arrays.toHexString(param, false);
+
+//   const obj = new JSON.Obj()
+//   obj.set('arg0', arg0Value)
+
+//   const result = <JSON.Obj>JSON.parse(SystemAPI.call('crypto.sha256', obj.stringify()))
+//   if (result.getString('result')!.isString) {
+//       return Arrays.Arrays.fromHexString(result.getString('result')!.valueOf()!)
+//   } else {
+//       //Never should happen
+//       throw new Error('Crypto - incorrect binding response')
+//   }
+// }
+
+class FullProof {
+  confirming_header: ConfirmingHeader | null = null
+
+  confirming_height: i64
+
+  version: string
+  vin: string
+  vout: string
+  locktime: string
+  tx_id: string
+  // pla: probably needs to be an array
+  intermediate_nodes: string
+  index: i64
+
+  constructor(confirming_height: i64, version: string, vin: string, vout: string, locktime: string, tx_id: string, intermediate_nodes: string, index: i64) {
+      this.confirming_height = confirming_height
+      this.version = version
+      this.vin = vin
+      this.vout = vout
+      this.locktime = locktime
+      this.tx_id = tx_id
+      this.intermediate_nodes = intermediate_nodes
+      this.index = index
+  }
+}
+
+class ConfirmingHeader {
+  raw: string
+  hash: string
+  height: i64
+  prevhash: string
+  merkle_root: string
+
+  constructor(raw: string, hash: string, height: i64, prevhash: string, merkle_root: string) {
+      this.raw = raw
+      this.hash = hash
+      this.height = height
+      this.prevhash = prevhash
+      this.merkle_root = merkle_root
+  }
+}
+
+function serializeConfirmingHeader(confirmingHeader: ConfirmingHeader, encoder: JSONEncoder): void {
+  encoder.pushObject("confirming_header");
+
+  encoder.setString('raw', confirmingHeader.raw);
+  encoder.setString('hash', confirmingHeader.hash);
+  encoder.setInteger('height', confirmingHeader.height);
+  encoder.setString('prevhash', confirmingHeader.prevhash);
+  encoder.setString('merkle_root', confirmingHeader.merkle_root);
+
+  encoder.popObject();
+}
+
+function serializeProof(proof: FullProof, encoder: JSONEncoder): void {
+  encoder.pushObject(null);
+
+  if (proof.confirming_header) {
+      serializeConfirmingHeader(proof.confirming_header!, encoder);
+  }
+  encoder.setString('confirming_height', proof.confirming_height.toString());
+  encoder.setString('version', proof.version);
+  encoder.setString('vin', proof.vin);
+  encoder.setString('vout', proof.vout);
+  encoder.setString('locktime', proof.locktime);
+  encoder.setString('tx_id', proof.tx_id);
+  encoder.setString('intermediate_nodes', proof.intermediate_nodes);
+  encoder.setInteger('index', proof.index);
+
+  encoder.popObject();
+}
+
+function deserializeProof(encoder: JSON.Obj): FullProof {
+  const confirming_height = encoder.getInteger('confirming_height')!.valueOf() as i64;
+  const version = getStringFromJSON(encoder, 'version');
+  const vin = getStringFromJSON(encoder, 'vin');
+  const vout = getStringFromJSON(encoder, 'vout');
+  const locktime = getStringFromJSON(encoder, 'locktime');
+  const tx_id = getStringFromJSON(encoder, 'tx_id');
+  const intermediate_nodes = getStringFromJSON(encoder, 'intermediate_nodes');
+  const index = encoder.getInteger('index')!.valueOf() as i64;
+
+  return new FullProof(confirming_height, version, vin, vout, locktime, tx_id, intermediate_nodes, index);
+}
+
+/**
+ * Method to validate a Bitcoin transaction proof
+ * @param proof The proof that should be validated
+ * @returns true if the proof is valid, false otherwise
+ */
+export function validateTxProofWrapper(proof: FullProof): bool {
+  let encoder = new JSONEncoder();
+  serializeProof(proof, encoder);
+  const serializedProof = encoder.toString();
+
+  return validateTxProof(serializedProof);
+}
+
+export function validateTxProof(proofStringJSON: string): bool {
+  const proofJSON = <JSON.Obj>JSON.parse(proofStringJSON);
+  const proof = deserializeProof(proofJSON);
+
+  const bundleHeaders: Map<i64, string> = getHeaders(calcKey(<i32>proof.confirming_height));
+  const header = bundleHeaders.get(proof.confirming_height);
+  const decodeHex = Arrays.Arrays.fromHexString(header);
+  const prevBlockLE = extractPrevBlockLE(decodeHex);
+  const prevBlock = Arrays.Arrays.toHexString(prevBlockLE, true);
+  const merkleRoot = Arrays.Arrays.toHexString(extractMerkleRootLE(decodeHex), true);
+  const headerHash = Arrays.Arrays.toHexString(hash256(decodeHex), true);
+
+  proof.confirming_header = new ConfirmingHeader(header, headerHash, proof.confirming_height, prevBlock, merkleRoot);
+
+  let encoder = new JSONEncoder();
+  serializeProof(proof, encoder);
+  const serializedProof = encoder.toString();
+  const obj = new JSON.Obj()
+  obj.set('arg0', serializedProof);
+  const proofDTO = obj.stringify();
+ 
+  const result = <JSON.Obj>JSON.parse(SystemAPI.call('bitcoin.validateSPVProof', proofDTO)) 
+
+  if (result.has('result') && result.getBool('result')!.isBool) {
+      const isValidProof = result.getBool('result')!.valueOf()
+      // console.log('the proof is ' + (isValidProof ? 'valid': 'invalid'))
+      return isValidProof;
+  } else {
+      //Never should happen
+      throw new Error('Crypto - incorrect binding response')
+  }
 }
